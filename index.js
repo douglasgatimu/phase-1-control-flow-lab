a13828b22ffd7e941e3b43f73c6897cfdc79e525
Douglas Gatimu <douglasgatimu@gmail.com>
2025-03-19 07:03:47 +0300

    Added conditionals.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if ((feet > 400) && (feet <= 2000)) {
+    return 'That will be twenty bucks.'
+  } else if ((feet > 2000) && (feet<=2500)) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return  'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch (tip) {
+    case 'generous': 
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+  return message;
 }
+
+//console.log(scuberGreetingForFeet(2501));
+//console.log(ternaryCheckCity('NYC'));
+console.log(switchOnCharmFromTip(' generous'));
